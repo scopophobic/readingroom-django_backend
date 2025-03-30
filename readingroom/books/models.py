4227a285a05e2597890dae9a5ee27b9acd5a8ed8
scopophobic <adithyanmadhu1234@gmail.com>
2025-03-30 23:32:59 +0530

    added books and genre models

--- readingroom/books/models.py
+++ readingroom/books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+
+class Book(models.Model):
+     # name, author, Published Date, discription, book_id
+    title = models.CharField(max_length=255)
+    authors = models.CharField(blank = True, null = True)
+    description = models.TextField(blank=True, null = True)
+    publication_date = models.DateField(blank=True, null=True)
+    cover_image_url = models.URLField(blank=True, null=True)
+    isbn = models.CharField(max_length=20, blank=True, null=True)
+    publisher = models.CharField(max_length=255, blank=True, null=True)
+    genres = models.ManyToManyField(Genre, blank=True)
+
+    google_book_id = models.CharField(max_length=255, unique = True, blank = True, null = True)
+
+    def __str__(self):
+        return self.title
+
